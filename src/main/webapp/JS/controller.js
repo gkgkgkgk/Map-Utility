fa661985ae11d1fb80956d760a6fee23e33e9100
Gavri Kepets <gkepets@gmail.com>
2017-08-25 13:55:21 -0400

    Using MapHilight Library

--- src/main/webapp/JS/controller.js
+++ src/main/webapp/JS/controller.js
@@ -1,7 +1,24 @@
-/**
- * 
- */
-$(document).ready(function(){
-	$('.map').maphilight();
+$(document).ready(function() {
+	
+	// initialize hilight
+	$(".map").maphilight({
+		strokeColor : "4CAF50",
+		strokeWidth : 2,
+		fillColor : '8BC34A',
+		fillOpacity : 0.25,
+		alwaysOn : true
+	});
+	
+	$('area').click(function(e) {
+		e.preventDefault();
+		console.log("click");
+		var data = $(this).data('maphilight') || {};
+		data.fillColor = 'F44336';
+		data.strokeColor = "FF5722";
+		$(this).data('maphilight', data).trigger('alwaysOn.maphilight');
+	});
+	
+	$("area").each(function(index) {
 
+	});
 });
